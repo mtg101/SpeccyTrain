bcfaa30bef80734b69c445cf83d33f3294b1a1a3
Russell Bulmer <russell.bulmer@gmail.com>
2025-06-07 00:21:13 +0100

    done udgs

---
+++ train_data.asm
@@ -0,0 +1,191 @@
+SCENE_CHARACTERS:
+	; RLE char, numTimes (max 255 - b is 8bit!), 0 terminated
+	; total 32*22=704 (ignoring channel 1 lower area for now)
+	defb	'*', 32
+	defb	'-', 32
+	defb	$86, 128
+	defb	C_SPACE, 33
+	defb	$84, 1
+	defb	$8C, 3
+	defb	$88, 1
+	defb	C_SPACE, 27
+	defb	$85, 1
+	defb	'Z', 1
+	defb	'X', 1
+	defb	'R', 1
+	defb	$8A, 1
+	defb	C_SPACE, 27
+	defb	$81, 1
+	defb	$83, 3
+	defb	$82, 1
+	defb	C_SPACE, 218
+	defb	C_UDG_1, 192
+	defb	0
+
+SCENE_ATTRS:
+	; RLE attr, numTime (max 255 - b is lower bit!), 0 terminated
+	; total 32*24=768 (ignoring channel 1 lower area for now)
+	defb	ATTR_RED_PAP, 200
+	defb	ATTR_CYN_PAP, 20
+	defb	ATTR_RED_PAP, 12
+	defb	ATTR_CYN_PAP, 20
+	defb	ATTR_RED_PAP, 6
+	defb	ATTR_YPBI, 3
+	defb	ATTR_RED_PAP, 3
+	defb	ATTR_CYN_PAP, 20
+	defb	ATTR_RED_PAP, 12
+	defb	ATTR_CYN_PAP, 20
+	defb	ATTR_RED_PAP, 12
+	defb	ATTR_CYN_PAP, 20
+	defb	ATTR_RED_PAP, 12
+	defb	ATTR_CYN_PAP, 20
+	defb	ATTR_RED_PAP, 12
+	defb	ATTR_CYN_PAP, 20
+	defb	ATTR_RED_PAP, 12
+	defb	ATTR_GRN_PAP, 20
+	defb	ATTR_RED_PAP, 10
+	defb	ATTR_BLK_PAP, 1
+	defb	ATTR_RED_PAP, 1
+	defb	ATTR_GRN_PAP, 20
+	defb	ATTR_RED_PAP, 1
+	defb	ATTR_BLK_PAP, 1
+	defb	ATTR_RED_PAP, 8
+	defb	ATTR_BLK_PAP, 1
+	defb	ATTR_RED_PAP, 1
+	defb	ATTR_GRN_PAP, 20
+	defb	ATTR_RED_PAP, 1
+	defb	ATTR_BLK_PAP, 1
+	defb	ATTR_RED_PAP, 8	
+	defb	ATTR_BLK_PAP, 1
+	defb	ATTR_RED_PAP, 22
+	defb	ATTR_BLK_PAP, 1
+	defb	ATTR_RED_PAP, 8	
+	defb	ATTR_BLK_PAP, 1
+	defb	ATTR_RED_PAP, 2
+	defb	ATTR_BLK_PAP, 18
+	defb	ATTR_RED_PAP, 2
+	defb	ATTR_BLK_PAP, 1
+	defb	ATTR_RED_PAP, 8
+	defb	ATTR_BLK_PAP, 1
+	defb	ATTR_RED_PAP, 3
+	defb	ATTR_BLK_PAP, 1
+	defb	ATTR_RED_PAP, 14
+	defb	ATTR_BLK_PAP, 1
+	defb	ATTR_RED_PAP, 3
+	defb	ATTR_BLK_PAP, 1
+	defb	ATTR_RED_PAP, 8
+	defb	ATTR_BLK_PAP, 3
+	defb	ATTR_RED_PAP, 18
+	defb	ATTR_BLK_PAP, 3
+	defb	ATTR_RED_PAP, 9
+	defb	ATTR_BLK_PAP, 1
+	defb	ATTR_RED_PAP, 20
+	defb	ATTR_BLK_PAP, 1
+	defb	ATTR_RED_PAP, 10
+	defb	ATTR_BLK_PAP, 1
+	defb	ATTR_RED_PAP, 20
+	defb	ATTR_BLK_PAP, 1
+	defb	ATTR_RED_PAP, 3
+
+
+	defb	ATTR_BLK_PAP, 64
+	defb	0
+
+
+NUM_UDGS			= 6
+
+UDG_DITHER	= $90
+
+UDG_FENCE			= $91
+UDG_FENCE_COL		= COL_RED
+
+UDG_HEDGE			= $92
+UDG_HEDGE_COL		= COL_GRN
+
+UDG_TREE_LOW		= $93
+UDG_TREE_LOW_COL	= COL_RED
+
+UDG_TREE_HIGH		= $94
+UDG_TREE_HIGH_GRN	= COL_GRN
+
+UDG_BUILDING		= $95
+UDG_BUILDING_COL	= COL_BLK
+
+
+
+UDGS:
+; UDG_DITHER
+	defb	%10101010
+	defb	%01010101
+	defb	%10101010
+	defb	%01010101
+	defb	%10101010
+	defb	%01010101
+	defb	%10101010
+	defb	%01010101
+
+; UDG_FENCE
+	defb	%00000000
+	defb	%00000000
+	defb	%00000000
+	defb	%00000000
+	defb	%01010101
+	defb	%11111111
+	defb	%10101010
+	defb	%10101010
+
+; UDG_HEDGE
+	defb	%00000000
+	defb	%00000000
+	defb	%00000000
+	defb	%00000000
+	defb	%00000000
+	defb	%01110100
+	defb	%01111110
+	defb	%11111111
+
+; UDG_TREE_LOW
+	defb	%00011000
+	defb	%00011000
+	defb	%00011000
+	defb	%00011100
+	defb	%00011000
+	defb	%00011000
+	defb	%00111100
+	defb	%01111100
+
+; UDG_TREE_HIGH
+	defb	%00011000
+	defb	%00111100
+	defb	%01111110
+	defb	%01111110
+	defb	%01111100
+	defb	%00111100
+	defb	%00111100
+	defb	%00011000
+
+; UDG_BUILDING
+	defb	%11111111
+	defb	%10000001
+	defb	%10000001
+	defb	%10000001
+	defb	%10000001
+	defb	%11111111
+	defb	%11111111
+	defb	%11111111
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- train_main.asm
+++ train_main.asm
@@ -2,13 +2,14 @@
 	org $8000
 	
 	INCLUDE "speccy_defs.asm"		; must be indented
+	INCLUDE "train_data.asm"
 	
 
 START:
 	EI								; need for halts
+	call	LOAD_UDGS
 	call	DRAW_SCENE
 	call	SETUP_BUILDINGS
-	
 	call	ANIMATE
 
 EXIT:
@@ -20,6 +21,13 @@ SETUP_BUILDINGS:
 ANIMATE:
 	halt
 	jr		ANIMATE
+
+LOAD_UDGS:
+	ld		de, UDG_START			; first UDG addr
+	ld		hl, UDGS				; my UDGs
+	ld		bc, NUM_UDGS * 8		; loop
+	LDIR
+	ret
    
 DRAW_SCENE:
 ; basic border
@@ -28,17 +36,6 @@ DRAW_SCENE:
 	
 ; clear screen
 	call	ROM_CLS					; so AT works, not just print at bottom of screen
-
-; set UDG
-	ld		hl, UDG_START			; load first UDG addr
-	ld		b, 4					; 2 lots of alternating pattern
-LOOP_UDG:	
-	ld		(hl), %10101010			; load alternate 1010 patters, 'unrolled' ;)
-	inc 	hl
-	ld		(hl), %01010101
-	inc 	hl
-	djnz	LOOP_UDG				
-	
 
 ; draw characters	
 	ld		hl, SCENE_CHARACTERS	; load addr of characters 
@@ -58,7 +55,6 @@ LOOP_RLE_CHAR:						; assume: num times not 0...
 	jr		LOOP_CHAR				; next RLE block
 	
 CHARS_DONE:
-
 ; draw attrs
 	ld		hl, SCENE_ATTRS
 	ld		de, ATTR_START			; de points to ATTR mem
@@ -81,98 +77,22 @@ ATTRS_DONE:
 DRAW_SCENE_DONE:
 	ret
 
-SCENE_CHARACTERS:
-	; RLE char, numTimes (max 255 - b is 8bit!), 0 terminated
-	; total 32*22=704 (ignoring channel 1 lower area for now)
-	defb	'*', 32
-	defb	'-', 32
-	defb	$86, 128
-	defb	C_SPACE, 33
-	defb	$84, 1
-	defb	$8C, 3
-	defb	$88, 1
-	defb	C_SPACE, 27
-	defb	$85, 1
-	defb	'Z', 1
-	defb	'X', 1
-	defb	'R', 1
-	defb	$8A, 1
-	defb	C_SPACE, 27
-	defb	$81, 1
-	defb	$83, 3
-	defb	$82, 1
-	defb	C_SPACE, 218
-	defb	C_UDG_1, 192
-	defb	0
+RNG: 								; uses first 8KiB ROM for pseudo
+	ld		hl,(SEED)        		; SEED is both seed and the random number
+    ld		a,h
+    and		$1F              		; keep it within first 8k of ROM.
+    ld		h,a
+    ld		a,(hl)           		; Get "random" number from location.
+    inc		hl              		; Increment pointer.
+    ld		(SEED),hl
+    ret
+SEED:
+	defw	0
 
-SCENE_ATTRS:
-	; RLE attr, numTime (max 255 - b is lower bit!), 0 terminated
-	; total 32*24=768 (ignoring channel 1 lower area for now)
-	defb	ATTR_RED_PAP, 200
-	defb	ATTR_CYN_PAP, 20
-	defb	ATTR_RED_PAP, 12
-	defb	ATTR_CYN_PAP, 20
-	defb	ATTR_RED_PAP, 6
-	defb	ATTR_YPBI, 3
-	defb	ATTR_RED_PAP, 3
-	defb	ATTR_CYN_PAP, 20
-	defb	ATTR_RED_PAP, 12
-	defb	ATTR_CYN_PAP, 20
-	defb	ATTR_RED_PAP, 12
-	defb	ATTR_CYN_PAP, 20
-	defb	ATTR_RED_PAP, 12
-	defb	ATTR_CYN_PAP, 20
-	defb	ATTR_RED_PAP, 12
-	defb	ATTR_CYN_PAP, 20
-	defb	ATTR_RED_PAP, 12
-	defb	ATTR_GRN_PAP, 20
-	defb	ATTR_RED_PAP, 10
-	defb	ATTR_BLK_PAP, 1
-	defb	ATTR_RED_PAP, 1
-	defb	ATTR_GRN_PAP, 20
-	defb	ATTR_RED_PAP, 1
-	defb	ATTR_BLK_PAP, 1
-	defb	ATTR_RED_PAP, 8
-	defb	ATTR_BLK_PAP, 1
-	defb	ATTR_RED_PAP, 1
-	defb	ATTR_GRN_PAP, 20
-	defb	ATTR_RED_PAP, 1
-	defb	ATTR_BLK_PAP, 1
-	defb	ATTR_RED_PAP, 8	
-	defb	ATTR_BLK_PAP, 1
-	defb	ATTR_RED_PAP, 22
-	defb	ATTR_BLK_PAP, 1
-	defb	ATTR_RED_PAP, 8	
-	defb	ATTR_BLK_PAP, 1
-	defb	ATTR_RED_PAP, 2
-	defb	ATTR_BLK_PAP, 18
-	defb	ATTR_RED_PAP, 2
-	defb	ATTR_BLK_PAP, 1
-	defb	ATTR_RED_PAP, 8
-	defb	ATTR_BLK_PAP, 1
-	defb	ATTR_RED_PAP, 3
-	defb	ATTR_BLK_PAP, 1
-	defb	ATTR_RED_PAP, 14
-	defb	ATTR_BLK_PAP, 1
-	defb	ATTR_RED_PAP, 3
-	defb	ATTR_BLK_PAP, 1
-	defb	ATTR_RED_PAP, 8
-	defb	ATTR_BLK_PAP, 3
-	defb	ATTR_RED_PAP, 18
-	defb	ATTR_BLK_PAP, 3
-	defb	ATTR_RED_PAP, 9
-	defb	ATTR_BLK_PAP, 1
-	defb	ATTR_RED_PAP, 20
-	defb	ATTR_BLK_PAP, 1
-	defb	ATTR_RED_PAP, 10
-	defb	ATTR_BLK_PAP, 1
-	defb	ATTR_RED_PAP, 20
-	defb	ATTR_BLK_PAP, 1
-	defb	ATTR_RED_PAP, 3
+BUILDING_BUF:
+	defs	24*10					; space for 10x20+4 window
 
 
-	defb	ATTR_BLK_PAP, 64
-	defb	0
 
 ; Deployment: Snapshot
    SAVESNA 	"speccy_train.sna", START
