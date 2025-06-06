f9a2fb2718af2457f21a5e87c62718cec3825c47
Russell Bulmer <russell.bulmer@gmail.com>
2025-06-06 18:06:04 +0100

    Basic train scene RLE'd

--- speccy_defs.asm
+++ speccy_defs.asm
@@ -1,26 +1,33 @@
 ; character codes 
-C_ENTER		= $0D
-C_AT		= $16
-C_UDG_1		= $90
-C_SPACE		= $20
+C_ENTER			= $0D
+C_AT			= $16
+C_UDG_1			= $90
+C_SPACE			= $20
 
 ; colours
-COL_BLK		= $0
-COL_BLU		= $1
-COL_RED		= $2
-COL_MAG		= $3
-COL_GRN		= $4
-COL_CYN		= $5
-COL_YEL		= $6
-COL_WHT		= $7
-COL_C_INK	= $10
-COL_C_PAP	= $11
-COL_C_FLA	= $12
-COL_C_BRI	= $13
+COL_BLK			= $0
+COL_BLU			= $1
+COL_RED			= $2
+COL_MAG			= $3
+COL_GRN			= $4
+COL_CYN			= $5
+COL_YEL			= $6
+COL_WHT			= $7
+COL_C_INK		= $10
+COL_C_PAP		= $11
+COL_C_FLA		= $12
+COL_C_BRI		= $13
+
+ATTR_RED_PAP	= %00010000
+ATTR_CYN_PAP	= %00101000
+ATTR_GRN_PAP	= %00100000
+
 
 ; ROM calls
-ROM_CLS		= $0DAF					; cls and open Channel 2 
-ROM_BORDER	= $229B					; set border to value in a
+ROM_CLS			= $0DAF					; cls and open Channel 2 
+ROM_BORDER		= $229B					; set border to value in a
 
 ; system vars
-UDG_START	= $FF58
+UDG_START		= $FF58
+ATTR_START		= $5800
+
--- train_main.asm
+++ train_main.asm
@@ -17,26 +17,90 @@ END:
   
  
 DRAW_SCENE:
+; basic border
+	ld		a, COL_CYN				; blue in a
+	call	ROM_BORDER				; sets border to val in a
+	
+; clear screen
+	call	ROM_CLS					; so AT works, not just print at bottom of screen
+
 ; draw characters
 
+LOOP_CHARS:
+	ld		hl, SCENE_CHARACTERS
+LOOP_CHAR:
+	ld		a, (hl)					; get char to display
+	cp		a, 0					; check it's not null
+	jr		z, CHARS_DONE			; if null we're done
+	inc		hl						; move to num times
+	ld		b, (hl)					; load b counter with num times to display
+LOOP_RLE_CHAR:						; assume: num times not 0...
+	push	af						; rst break a, so save
+	rst		$10						; print the char
+	pop		af						; resotre a
+	djnz	LOOP_RLE_CHAR			; loop until done
+	
+	inc 	hl						; next char (null check comes at start of loop)
+	jr		LOOP_CHAR				; next RLE block
+	
+CHARS_DONE:
 
-; draw 
+; draw attrs
+	ld		hl, SCENE_ATTRS
+	ld		de, ATTR_START			; de points to ATTR mem
+LOOP_ATTR:
+	ld		a, (hl)					; get attr to use
+	cp		a, 0					; check it's not null
+	jr		z, ATTRS_DONE			; if null we're done
+	inc		hl						; move to num times
+	ld		b, (hl)					; load b counter with num times to display
+LOOP_RLE_ATTR:						; assume: num times not 0...
+	ld		(de), a					; put the attr on screen
+	inc		de
+	djnz	LOOP_RLE_ATTR			; lool until done
+	
+	inc		hl						; next attr (null check comes at start of loop)
+	jr		LOOP_ATTR				; next RLE block
+	
+ATTRS_DONE:
 
+DRAW_SCENE_DONE:
 	ret
 
 SCENE_CHARACTERS:
-	; RLE char, numTimes (max 255 - b is 8bit!)
+	; RLE char, numTimes (max 255 - b is 8bit!), 0 terminated
 	; total 32*22=704 (ignoring channel 1 lower area for now)
-	defb	C_SPACE, 255
-	defb	C_SPACE, 255
-	defb	C_SPACE, 194
+	defb	'*', 32
+	defb	'-', 32
+	defb	0
 
 SCENE_ATTRS:
-	; RLE attr, numTime (max 255 - b is lower bit!)
+	; RLE attr, numTime (max 255 - b is lower bit!), 0 terminated
 	; total 32*24=768 (ignoring channel 1 lower area for now)
+	defb	ATTR_RED_PAP, 200
+	defb	ATTR_CYN_PAP, 20
+	defb	ATTR_RED_PAP, 12
+	defb	ATTR_CYN_PAP, 20
+	defb	ATTR_RED_PAP, 12
+	defb	ATTR_CYN_PAP, 20
+	defb	ATTR_RED_PAP, 12
+	defb	ATTR_CYN_PAP, 20
+	defb	ATTR_RED_PAP, 12
+	defb	ATTR_CYN_PAP, 20
+	defb	ATTR_RED_PAP, 12
+	defb	ATTR_CYN_PAP, 20
+	defb	ATTR_RED_PAP, 12
+	defb	ATTR_CYN_PAP, 20
+	defb	ATTR_RED_PAP, 12
+	defb	ATTR_GRN_PAP, 20
+	defb	ATTR_RED_PAP, 12
+	defb	ATTR_GRN_PAP, 20
+	defb	ATTR_RED_PAP, 12
+	defb	ATTR_GRN_PAP, 20
+	defb	ATTR_RED_PAP, 255
+	defb	ATTR_RED_PAP, 5
+	defb	0
 
- 
-  
 ; Deployment: Snapshot
    SAVESNA 	"speccy_train.sna", START
    
